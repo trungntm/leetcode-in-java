f9e8ba9ed62fb4f813914df8a1dc17f9e8ff9717
Nguyễn Tạ Minh Trung <trungntm.97@gmail.com>
2026-09-09 20:08:57 +0700

    15. Three sum

---
+++ algorithms/medium/15.3sum/src/main/java/com/trungtmnguyen/ThreeSum.java
@@ -0,0 +1,10 @@
+package com.trungtmnguyen;
+
+public class ThreeSum {
+
+  public static void main(String[] args) {
+    int[] nums = {-1, 0, 1, 2, -1, -4};
+    Solutions solutions = new Solutions();
+    System.out.println(solutions.threeSum(nums));
+  }
+}
